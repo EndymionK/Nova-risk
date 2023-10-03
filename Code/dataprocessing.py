d01ab8c9332b50e6a7abfd88a9f26defcaf6fafd
EndymionK <andres.arroyavec1@udea.edu.co>
2023-10-03 12:44:03 -0500

    Improving risk calculation

--- Code/dataprocessing.py
+++ Code/dataprocessing.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 df = pd.read_csv('C:/Proyectos programación/Nova-risk/Databases/hygdata_v35.csv')
 
-# Definir una función hipotética para calcular la probabilidad de nova o supernova
+# Definir una función para calcular la probabilidad de nova o supernova
 def calcular_probabilidad_nova_supernova(row):
+    magnitud_absoluta = row['absmag']  # Magnitud absoluta
     tipo_espectral = row['spect']  # Tipo espectral
+    velocidad_radial = row['rv']  # Velocidad radial
+    luminosidad = row['lum']  # Luminosidad
     
     # Verificar si el valor en la columna "spect" es una cadena de texto
     if isinstance(tipo_espectral, str):
@@ -22,21 +25,19 @@ def calcular_probabilidad_nova_supernova(row):
         clase_principal = 'X'  # Valor por defecto para clase principal
         subclase = 0  # Valor por defecto para subclase
     
-    # Hipotético cálculo de la probabilidad de nova o supernova (sin considerar magnitud absoluta)
-    probabilidad = 0.1  # Valor inicial
+    # Calcular la probabilidad hipotética de nova o supernova
+    # Puedes ajustar esta fórmula según tus propios criterios
+    probabilidad_nova_supernova = (
+        (subclase * 0.3) +  # Contribución de la subclase
+        (magnitud_absoluta * 0.2) +  # Contribución de la magnitud absoluta
+        (velocidad_radial * 0.2) +  # Contribución de la velocidad radial
+        (luminosidad * 0.3)  # Contribución de la luminosidad
+    )
     
-    # Asignar un mayor valor de probabilidad a clases espectrales más masivas
-    if clase_principal in ['O']:
-        probabilidad += 15
-    elif clase_principal in ['B']:
-        probabilidad += 10
-    elif clase_principal in ['A']:
-        probabilidad += 5
+    # Escalar la probabilidad a un valor entre 1 y 100
+    probabilidad_nova_supernova = max(1, min(100, probabilidad_nova_supernova))
     
-    # Asignar un mayor valor de probabilidad a subclases más bajas
-    probabilidad += subclase * 5  # Por ejemplo, aumentamos en 5 por cada unidad de subclase
-    
-    return probabilidad
+    return probabilidad_nova_supernova
 
 # Aplicar la función para calcular la probabilidad a cada fila del DataFrame
 df['probabilidad_nova_supernova'] = df.apply(calcular_probabilidad_nova_supernova, axis=1)
@@ -78,3 +79,4 @@ def analizar_columna_csv(nombre_archivo, nombre_columna):
 nombre_archivo = 'C:/Proyectos programación/Nova-risk/Databases/hygdatarisk.csv'
 nombre_columna = 'spect'
 analizar_columna_csv(nombre_archivo, nombre_columna)
+
